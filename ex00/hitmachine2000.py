5225c47426d547178aa56ad169b5129399366680
wejdene <louhichi.wejdene@gmail.com>
2021-06-03 10:10:08 +0200

    Rendu Day02

---
+++ ex00/hitmachine2000.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+print("Moi je m'appelle 'Lolita'")
+print("Lo ou bien Lola, du pareil au même")
+print("Moi je m'appelle 'Lolita'")
+print("Quand je rêve aux Loups, c'est Lola qui saingne")
+print("[...]")
+print("C'est ma pas ma faute")
+print("Et quand je donne ma langue aux chats je vois les autres")
+print("Tout prêts à se jeter sur moi, c'est pas ma faute à moi")
+print("Si j'entends tout autour de moi")
+print("L.O.L.I.T.A, moi 'Lolita'")
+
+
